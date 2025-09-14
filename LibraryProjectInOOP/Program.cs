19356b3d1695b982b1e8ba9a75c27a7689780662
Heba Abou El-EZZ <bebaadel656@gmail.com>
2025-09-14 02:12:14 -0700

    Update Program.cs

--- LibraryProjectInOOP/Program.cs
+++ LibraryProjectInOOP/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,45 +14,44 @@ namespace LibraryProjectInOOP
             Book book2 = new Book("English", 22222, "Adel", "Education", 1000, "2022");
 
             Owner owner = new Owner("yara", "3333", "owner@mylibrary.com");
-
             Operation operation = new Operation(444, "Menna", "6666", "operation@mylibrary.com");
-
             Customer customer = new Customer("Heba", 999777, "0100000", 2, "3333333", "customer@yara.com");
-          
+
+            List<Book> books = new List<Book> { book1, book2 };
+
+           
+            Console.WriteLine("Books:");
+            foreach (Book book in books)
             {
-
-
-                List<Book> books = new List<Book> { book1, book2 };
-
-                Console.WriteLine("Books is:");
-                foreach (Book book in books)
-                {
-                    book.BookInfo();
-
-                }
-
-                Console.WriteLine("=====================================");
-
-                Console.WriteLine("owner information ");
-                owner.OwnerInfo();
-                Console.WriteLine("=====================================");
-
-                Console.WriteLine("operation information is:");
-                operation.OperationInfo();
-                Console.WriteLine("=====================================");
-
-                Console.WriteLine("customer information is:");
-                customer.CustomerInfo();
-                Console.WriteLine("=====================================");
-                customer.BorrowedBookInfo();
-                customer.BorrowBook(book2);
-               customer.BorrowBook(book1);
-                customer.BorrowBook(book2);
-                customer.ReturnBook(book2);
-
-                Console.ReadLine();
-
+                book.BookInfo();
             }
+            Console.WriteLine("=====================================");
+
+            
+            Console.WriteLine("Owner information:");
+            owner.UserInfo();
+            Console.WriteLine("=====================================");
+
+            
+            Console.WriteLine("Operation information:");
+            operation.UserInfo();
+            Console.WriteLine("=====================================");
+
+            
+            Console.WriteLine("Customer information:");
+            customer.UserInfo();
+            Console.WriteLine("=====================================");
+
+            customer.BorrowedBookInfo();
+            customer.BorrowBook(book2);
+            customer.BorrowBook(book1);
+            customer.BorrowBook(book2);
+            customer.ReturnBook(book2);
+
+           
+           
+
+            Console.ReadLine();
         }
     }
 }
